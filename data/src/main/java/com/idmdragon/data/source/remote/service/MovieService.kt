260c3b198a3218c3200aae2747bc88418f17d381
Ilham Dwi <ilham.uklison@gmail.com>
2022-03-12 00:49:39 +0700

    feat(movie): Setup end point for movie remote

--- data/src/main/java/com/idmdragon/data/source/remote/service/MovieService.kt
+++ data/src/main/java/com/idmdragon/data/source/remote/service/MovieService.kt
@@ -1,7 +1,35 @@
 package com.idmdragon.data.source.remote.service
 
+import com.idmdragon.data.source.remote.response.GeneralResponse
+import com.idmdragon.data.source.remote.response.MovieResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieService {
 
+    @GET("/3/movie/now_playing")
+    suspend fun getMovieNowPlaying(
+        @Query("api_key")
+        api_key: String
+    ): GeneralResponse<MovieResponse>
 
+    @GET("/3/movie/popular")
+    suspend fun getMoviePopular(
+        @Query("api_key")
+        api_key: String
+    ): GeneralResponse<MovieResponse>
+
+    @GET("/3/movie/popular")
+    suspend fun getMovieTopRated(
+        @Query("api_key")
+        api_key: String
+    ): GeneralResponse<MovieResponse>
+
+    @GET("/3/movie/popular")
+    suspend fun getMovieUpcoming(
+        @Query("api_key")
+        api_key: String
+    ): GeneralResponse<MovieResponse>
 
 }

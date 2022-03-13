f75faef289dc60d5e17d0c6fe5b8601a9529ebbf
Ilham Dwi <ilham.uklison@gmail.com>
2022-03-13 16:42:53 +0700

    feat(main): improve ux when user click onBackPress

--- app/src/main/java/com/idmdragon/movieplay/ui/main/MainActivity.kt
+++ app/src/main/java/com/idmdragon/movieplay/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.idmdragon.movieplay.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,6 +14,7 @@ import com.idmdragon.movieplay.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +29,19 @@ class MainActivity : AppCompatActivity() {
 
         binding.ibSearch.setOnClickListener {
             startActivity(Intent(this, Class.forName(PAGE_SEARCH_ACTIVITY)))
+        }
+    }
+
+    override fun onBackPressed() {
+        val t = System.currentTimeMillis()
+        if (t - backPressedTime > 2000) {
+            backPressedTime = t
+            Toast.makeText(
+                this, getString(R.string.text_press_one_again_to_exit),
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            finishAffinity()
         }
     }
 }
